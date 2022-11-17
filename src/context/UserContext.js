6a43405fe6a03cbbd74a02785fe3b481026721b2
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-11-17 10:22:05 -0400

    token validate and logout

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,6 +1,6 @@
-//import { getSuggestedQuery } from '@testing-library/react';
-import React, { createContext, useState } from 'react';
-import { TOKEN_POST, USER_GET, USER_REGISTER } from '../api';
+import React, { createContext, useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET, USER_REGISTER } from '../api';
 import  useFetch from '../Hooks/useFetch';
 
 export const UserContext = createContext();
@@ -8,10 +8,33 @@ export const UserContext = createContext();
 export const UserStorage = ({children}) => {
   const [data, setData] = useState(null);
   const [login, setLogin] = useState(false);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const {loading: loadingFetch, error: errorFetch, request} = useFetch();
 
-  const {loading, error, request} = useFetch();
+  useEffect(() => {
+    async function autoLogin () {
+      try{
+        setLoading(true);
+        setError(null);
+        const token = window.localStorage.getItem('token');
+        if(!token) return;
+        
+        const {url, options} = TOKEN_VALIDATE_POST(token);
+        const response = await request(url, options);
+
+        if(response.json.code !== 'jwt_auth_valid_token') throw new Error('Token inválido!');
+        await getUser(token);
+      } catch (e) {
+        userLogout();
+      } finally {
+        setLoading(false);
+        setError(null);
+      }
+
+    }
+    autoLogin();
+  }, [request]);
 
   async function getUser (token) {
     const {url, options} = USER_GET(token);
@@ -23,45 +46,69 @@ export const UserStorage = ({children}) => {
   }
 
   async function userLogin (username, password) {
-    const token = window.localStorage.getItem('token'); 
-    if(token){
-      getUser(token);
-    }else{
+    try{
+      setLoading(true);
+      setError(null);
+      const token = window.localStorage.getItem('token'); 
+      if(token){
+        await getUser(token);
+      }else{
+        setLogin(false);
+      }
+  
+      const {url, options} = TOKEN_POST({username, password});
+      const response = await request(url, options);
+      if(!response.response.ok){
+        if(response.statusText) throw new Error(response.statusText);
+        throw new Error('Usuário inválido!');
+      }
+      const data = await response.json;
+      if(data.token){
+          window.localStorage.setItem('token', data.token);
+          await getUser(data.token);
+      }
+    }catch(err){
+      setError(err.message);
       setLogin(false);
-    }
-
-    const {url, options} = TOKEN_POST({username, password});
-    const response = await request(url, options);
-    console.log(error);
-    const data = await response.json;
-    if(data.token){
-        window.localStorage.setItem('token', data.token);
-    
-        getUser(data.token);
+    }finally{
+      setLoading(false); 
     }
   }
 
   async function userRegister (username, password, email) {
-    const {url, options} = USER_REGISTER({username, password, email});
-    const response = await request(url, options);
-    const json = await response.json;
-    if(!json.code || json.code !== 'error') {
-      console.log(json);
+    try{
+      setError(null);
+      setLoading(true);
+      const {url, options} = USER_REGISTER({username, password, email});
+      const response = await request(url, options);
+      if(!response.response.ok) {
+        if(response.statusText) throw new Error(response.statusText);
+        throw new Error('Dados inválidos e/ou já usados!');
+      }
+    }catch(e) {
+      setError(e.message);
+    }finally{
+      setLoading(false);
     }
   }
 
-  async function userLogoff () {
-    
-    //setLogin(false);
+  async function userLogout () {
+    setLogin(false);
+    setData(null);
+    setError(null);
+    setLoading(false);
+    window.localStorage.removeItem('token');
   }
 
 return (
   <UserContext.Provider value={{
     userLogin,
     userRegister,
-    userLogoff,
+    userLogout,
     data,
-    login
+    login,
+    loading,
+    error
   }}>
 
     {children}
