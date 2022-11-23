8d8c1379ea42bfc6f3a761434cfc0810c21aac5b
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-11-23 09:49:00 -0400

    infinite scroll feed

--- src/components/Feed/components/FeedPhotos.js
+++ src/components/Feed/components/FeedPhotos.js
@@ -7,16 +7,20 @@ import Loading from '../../../Helper/Loading';
 import Error from '../../../Helper/Error';
 import FeedPhotosItem from './FeedPhotosItem';
 
-const FeedPhotos = ({user, setModal}) => {
+const FeedPhotos = ({user, page, setModal, setInfinite}) => {
   const {data, loading, error, request} = useFetch();
+  const total = 6
   
   useEffect(() => {
     async function fetchPhotos () {
-      const {url, options} = PHOTOS_GET({ page: 1, total: 10, user });
-      await request(url, options);
+      const {url, options} = PHOTOS_GET({ page, total, user });
+      const {response, json} = await request(url, options);
+      if(response && response.ok && json.length < total) setInfinite(false);
     }
+
     fetchPhotos();
-  }, [request, user]);
+
+  }, [request, user, setInfinite]);
 
   if(error) return <Error error={error} />
   if(loading) return <Loading />
--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -1,16 +1,55 @@
 import './index.css';
 import FeedModal from './components/FeedModal';
 import FeedPhotos from './components/FeedPhotos';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Feed = ({user}) => {
   const [modal, setModal] = useState(null);
+  const [pages, setPages] = useState([ 1 , 2 ]);
+  const [infinite, setInfinite] = useState(true);
+
+  useEffect(() => {
+    let wait = false;
+
+    function infiniteScroll () {
+      if(infinite) {
+        const scrollY = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+  
+        if(scrollY > height * .95 && !wait) {
+          setPages(pages => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => { wait = false }, 500);
+        } 
+      }
+    }
+
+    window.addEventListener('wheel', infiniteScroll);
+    window.addEventListener('scroll', infiniteScroll);
+
+    return () => { // to clean event
+      window.removeEventListener('wheel', infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
+
+    }
+  }, [infinite]);
+
   return (
     <div className="feed">
       {modal && 
         <FeedModal photo={modal} setModal={setModal}/>
       }
-      <FeedPhotos user={user} setModal={setModal} />
+
+      {pages.map(page => (
+         <FeedPhotos
+           user={user}
+           key={page}
+           page={page}
+           setModal={setModal}
+           setInfinite={setInfinite}
+         />
+      ))}
+      {!infinite && <p className='feed-end'>Não existem mais postagens.</p>}
     </div>
   )
 }
